607f7027a76b4f3f2901ea45cd81107bdfd14e86
Smit Talashilkar <smittalashilkar@gmail.com>
2021-07-17 19:39:08 +0530

    package update

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  //   plugins: [
-  //     "babel-plugin-annotate-pure-calls",
-  //     "babel-plugin-dev-expression",
-  //     ["@babel/plugin-proposal-class-properties", { loose: true }],
-  //     "babel-plugin-macros",
-  //   ],
   presets: [
     "@babel/preset-typescript",
     "@babel/preset-react",
