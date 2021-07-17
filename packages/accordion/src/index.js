f04904accd95ddbc752283d73b3881f1c6e3b957
Smit Talashilkar <smittalashilkar@gmail.com>
2021-07-17 19:41:02 +0530

    component update

--- packages/accordion/src/index.js
+++ packages/accordion/src/index.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Accordian() {
-  return (
-    <div>
-      Accordian Component
-    </div>
-  )
+  return <div>Accordian Component New</div>;
 }
